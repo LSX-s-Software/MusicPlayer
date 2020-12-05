5767ddd0c315b67fb2675f9a91b58b7cec3f787a
LSX-s-Software <2682191498@qq.com>
2020-12-05 19:44:00 +0800

    initial commit

---
+++ player1/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  player1
+//
+//  Created by 朱俊泽 on 2020/3/21.
+//  Copyright © 2020 朱俊泽. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+
+
+class ViewController: UIViewController {
+    @IBOutlet weak var itemImageView: UIImageView!
+    @IBOutlet weak var blurImageView: UIImageView!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var animationImageView: UIImageView!
+    @IBOutlet weak var playImageView: UIImageView!
+    var isPlayed:Bool = false
+    var audioPlayer:AVPlayer?
+    var playerItem:AVPlayerItem?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        playImageView.layer.opacity = 0
+        itemImageView.layer.cornerRadius = 100
+        //旋转
+        rotationAnimation(theView: itemImageView, direction: true)
+        rotationAnimation(theView: playImageView, direction: false)
+        //模糊
+        blurEffect(theView: blurImageView)
+        
+        //设置音频
+        audioPlayer = AVPlayer()
+        let url:URL = Bundle.main.url(forResource: "1", withExtension: "mp3")!
+        playerItem = AVPlayerItem(url: url)
+        audioPlayer?.replaceCurrentItem(with: playerItem)
+        
+    }
+    
+    @IBAction func playOrPause(_ sender: Any) {
+        if isPlayed {
+            audioPlayer?.pause()
+            self.playButton.setBackgroundImage(UIImage(systemName: "pause.circle.fill"), for: .normal)
+            //透明度改变
+            opacityAnimation(theView: playImageView, direction: true)
+            //缩放
+            scaleAnimation(theView: animationImageView)
+            playImageView.layer.opacity = 0
+            
+        }
+        else{
+            audioPlayer?.play()
+            self.playButton.setBackgroundImage(UIImage(systemName: "play.circle.fill"), for: .normal)
+            opacityAnimation(theView: playImageView, direction: false)
+            scaleAnimation(theView: animationImageView)
+            playImageView.layer.opacity = 1
+        }
+        isPlayed.toggle()
+    }
+}
+
---
+++ player1/animation.swift
@@ -0,0 +1,68 @@
+//
+//  animation.swift
+//  player1
+//
+//  Created by 朱俊泽 on 2020/3/21.
+//  Copyright © 2020 朱俊泽. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//在主控制器中扩展自定义动画
+extension ViewController{
+    
+    //旋转动画，direction旋转旋转方向
+    func rotationAnimation(theView: UIImageView,direction:Bool){
+        let animation = CABasicAnimation(keyPath: "transform.rotation.z")
+        animation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+        if direction {
+            animation.toValue = 2*Double.pi
+        }
+        else{
+            animation.toValue = -2*Double.pi
+        }
+        animation.duration = 16
+        animation.repeatCount = HUGE
+        animation.autoreverses = false
+        theView.layer.add(animation, forKey: "roatationAnimation")
+        
+    }
+    
+    //模糊效果
+    func blurEffect(theView:UIImageView){
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.frame = self.view.frame
+        theView.addSubview(blurView)
+    }
+    
+    //透明动画，direction选择显现还是隐藏
+    func opacityAnimation(theView:UIImageView,direction:Bool) {
+        let animation = CABasicAnimation(keyPath: "opacity")
+        
+        if direction {
+            animation.fromValue = 0
+            animation.toValue = 1
+            
+        }else{
+            animation.fromValue = 1
+            animation.toValue = 0
+        }
+        animation.duration = 0.3
+        animation.autoreverses = true
+        theView.layer.add(animation, forKey: "addLayeranimationOpacity")
+        
+    }
+    
+    //2倍缩放动画
+    func scaleAnimation(theView:UIImageView) {
+        let animation = CABasicAnimation(keyPath: "transform.scale")
+        animation.fromValue = 1
+        animation.toValue = 2
+        animation.duration = 0.3
+        animation.repeatCount = 1
+        animation.autoreverses = true
+        theView.layer.add(animation, forKey: "addLayerAnimationScale")
+    }
+}
