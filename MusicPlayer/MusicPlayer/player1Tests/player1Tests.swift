62a47d64a11441879b0006eb9ba33d74566c1190
ly-syx <72655500+ly-syx@users.noreply.github.com>
2020-12-11 22:16:46 +0800

    Add files via upload

---
+++ MusicPlayer/MusicPlayer/player1Tests/player1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  player1Tests.swift
+//  player1Tests
+//
+//  Created by 朱俊泽 on 2020/3/21.
+//  Copyright © 2020 朱俊泽. All rights reserved.
+//
+
+import XCTest
+@testable import player1
+
+class player1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
